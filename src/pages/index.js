d32007fd7683f3017c18bff097e6f2f2f466061b
Natasha DeCoste <natasha.decoste@gmail.com>
2020-03-30 21:25:32 -0400

    add built by section

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,18 @@ import Shell from "../components/shell"
 import Terminal from "react-console-emulator"
 import Loader from "./../components/loader"
 
+// images
 import image from "./../assets/profile1.jpg";
 import Dribble from 'react-share-icons/lib/Dribbble';
 import Twitter from 'react-share-icons/lib/Twitter';
 import SvgGithub from './../assets/github'
 
 
+// technologies
+import gatsby from "./../assets/project-assets/technologies/gatsby-icon.png";
+import react from "./../assets/project-assets/technologies/react.png";
+
+// styles
 import "./../styles/mainpage.scss"
 import "./../styles/global.scss"
 
@@ -73,47 +79,58 @@ class IndexPage extends React.Component {
       <Layout>
         <div className="page-body">
           <div className="header-bar"></div>
-            <div className="information">
-              <div className="text-content">
-                <h1 className="name">Natasha DeCoste</h1>
-                <p className="title">ux/ui developer at IBM</p>
-                <div className="icons-social">
-                  <a href="https://dribbble.com/natashadecoste"><Dribble className="neo shares-dribble"></Dribble></a>
-                  <a href="https://twitter.com/codebunny2"><Twitter className="neo shares-twitter"></Twitter></a>
-                  <a href="https://github.com/natashadecoste"><SvgGithub className="neo" height="100%" width="1.5em" fill="white"></SvgGithub></a>
-                </div>
+          <div className="information">
+            <div className="text-content">
+              <h1 className="name">Natasha DeCoste</h1>
+              <p className="title">ux/ui developer at IBM</p>
+              <div className="icons-social">
+                <a href="https://dribbble.com/natashadecoste"><Dribble className="neo shares-dribble"></Dribble></a>
+                <a href="https://twitter.com/codebunny2"><Twitter className="neo shares-twitter"></Twitter></a>
+                <a href="https://github.com/natashadecoste"><SvgGithub className="neo" height="100%" width="1.5em" fill="white"></SvgGithub></a>
               </div>
             </div>
+          </div>
           <div className="graphics">
             <img className="headshot" src={image}></img>
-              <div className="terminals">
-                <Shell
-                  id="looping-shell"
-                  height="150px"
-                >
-                  <Loader />
-                </Shell>
-                <Shell
-                  id="interactive-shell"
-                  height="250px"
-                  width="70%"
-                >
-                  <Terminal
-                    style={{ background: "none", height: "100%", minWidth: "unset", minHeight: "unset" }}
-                    inputClassName={`my-terminal-input`}
-                    contentStyle={{ padding: "none" }}
-                    ref={(c) => this.terminal = c}
-                    commands={commands}
-                    noDefaults={true}
-                    noHistory={false}
-                    promptLabel={'me@Desktop:~$'}
-                  />
-                </Shell>
-              </div>
+            <div className="terminals">
+              <Shell
+                id="looping-shell"
+                height="150px"
+              >
+                <Loader />
+              </Shell>
+              <Shell
+                id="interactive-shell"
+                height="250px"
+                width="70%"
+              >
+                <Terminal
+                  style={{ background: "none", height: "100%", minWidth: "unset", minHeight: "unset" }}
+                  inputClassName={`my-terminal-input`}
+                  contentStyle={{ padding: "none" }}
+                  ref={(c) => this.terminal = c}
+                  commands={commands}
+                  noDefaults={true}
+                  noHistory={false}
+                  promptLabel={'me@Desktop:~$'}
+                />
+              </Shell>
+            </div>
           </div>
         </div>
         <ProjectSection></ProjectSection>
         <SEO title="Natasha DeCoste" />
+        <div className="built-with">
+          <div className="image-row">
+            <div className="badge neo-inner">
+              <img src={react} />
+            </div>
+            <div className="badge neo-inner">
+              <img src={gatsby}></img>
+            </div>
+          </div>
+          <p>website powered by</p>
+        </div>
       </Layout>
     )
   }
